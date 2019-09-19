3c918646480a1a128c3cf12b8286e4fb52f73ab9
Rahul Barhate <rahulbarhate97@gmail.com>
2019-09-19 15:14:55 +0530

    Add files via upload

---
+++ solutions/path_sum.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) 
+    {
+        if(root == NULL)
+        {
+            return false;
+        }
+        
+        if(root->left == NULL && root->right == NULL)
+        {
+            if(root->val == sum)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        
+        if(hasPathSum(root->left, sum - root->val))
+        {
+            return true;
+        }
+        
+        if(hasPathSum(root->right, sum - root->val))
+        {
+            return true;
+        }
+        
+        return false;
+        
+    }
+};
