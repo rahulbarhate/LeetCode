d96090967e82d2789bda815d8e8e46e0f72a9e0b
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-14 12:41:17 +0530

    Create climbing_stairs_brute.java

---
+++ solutions/climbing_stairs_brute.java
@@ -0,0 +1,40 @@
+/*
+
+Approach 1: Brute Force
+Algorithm
+
+In this brute force approach we take all possible step combinations i.e. 1 and 2, at every step. 
+At every step we are calling the function climbStairsclimbStairs for step 11 and 22, and return the sum of returned values 
+of both functions.
+
+```java
+climbStairs(i,n)=(i + 1, n) + climbStairs(i + 2, n)
+```
+
+where i defines the current step and nn defines the destination step.
+
+*/
+
+class Solution 
+{
+    public int climbStairs(int n)
+    {
+        return climb_Stairs(0,n);
+    }
+    
+    
+    public int climb_Stairs(int i, int n)
+    {
+        if(i>n)
+        {
+            return 0;
+        }
+        
+        if(i==n)
+        {
+            return 1;
+        }
+        
+        return climb_Stairs(i+1,n) + climb_Stairs(i+2, n);
+    }
+}
