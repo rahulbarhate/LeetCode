4e15a338c0411a5511d453b4ac0066b4089e20df
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-16 19:15:54 +0530

    Create binary_tree_tilt.java

---
+++ solutions/binary_tree_tilt.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution 
+{
+    int tilt = 0;
+    public int findTilt(TreeNode root)
+    {
+        traverse(root);
+        return tilt;
+    }
+    
+    public int traverse(TreeNode root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        
+        int left = traverse(root.left);
+        int right = traverse(root.right);
+        tilt += Math.abs(left-right);
+        return left + right + root.val;
+    }
+}
