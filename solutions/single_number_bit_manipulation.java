8fd13a2687a6a2c923a71c7cf7eda3d5e36ee55c
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-17 14:47:19 +0530

    Create single_number_bit_manipulation.java

---
+++ solutions/single_number_bit_manipulation.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int singleNumber(int[] nums)
+    {
+        int a = 0;
+        for(int i:nums)
+        {
+            a ^= i;
+        }
+        
+        return a;
+    }
+}
