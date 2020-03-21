c6446c3b63c7aaeb8073cb284ce28d7c7fb98fcd
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-21 14:09:51 +0530

    Create contains_duplicate_hashset.java

---
+++ solutions/solutions/contains_duplicate_hashset.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public boolean containsDuplicate(int[] nums) 
+    {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for(int x:nums)
+        {
+            if(set.contains(x))
+                return true;
+            set.add(x);
+        }
+        
+        return false;
+    }
+}
