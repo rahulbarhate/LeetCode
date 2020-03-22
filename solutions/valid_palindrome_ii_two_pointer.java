7882d04aa2e63a3079818eb6c9c8bc302e22f720
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-22 16:22:23 +0530

    Create valid_palindrome_ii_two_pointer.java

---
+++ solutions/valid_palindrome_ii_two_pointer.java
@@ -0,0 +1,36 @@
+class Solution 
+{
+    public boolean validPalindrome(String s)
+    {
+        int i=0;
+        int j=s.length()-1;
+        
+        while(i<j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+            {
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }
+            
+            i++;j--;
+        }
+        
+        return true;
+    }
+    
+    
+    public boolean isPalindrome(String s, int i, int j)
+    {
+        while(i<j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+            {
+                return false;
+            }
+            
+            i++; j--;
+        }
+        
+        return true;
+    }
+}
