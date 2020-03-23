492a744cdb5352b2bf77386bc689ab77429845a5
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-23 14:57:19 +0530

    Create linked_list_cycle_two_pointer.java

---
+++ solutions/linked_list_cycle_two_pointer.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode
+ {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) 
+       {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution
+{
+    public boolean hasCycle(ListNode head) 
+    {
+        if(head == null || head.next == null)
+        {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(slow != fast)
+        {
+            if(fast == null || fast.next == null)
+            {
+                return false;
+            }
+            
+            slow = slow.next;
+            false = fast.next.next;
+        }
+        
+        return true;
+    }
+}
