ed4421882b21804895a7cb93d5e796ff51603bc2
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-23 20:26:07 +0530

    Create find_the_duplicate_number_sorting.java

---
+++ solutions/find_the_duplicate_number_sorting.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int findDuplicate(int[] nums)
+    {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1;i++)
+        {
+            if(nums[i] == nums[i+1])
+                return nums[i];
+        }
+        
+        return -1;
+    }
+}
