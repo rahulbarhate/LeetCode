a187f8313c53a53d51a56dcc68e0bec7a3259772
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-25 13:58:04 +0530

    Create two_sum_ii.java

---
+++ solutions/two_sum_ii.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int[] twoSum(int[] numbers, int target)
+    {
+        int low = 0, high = numbers.length-1;
+        while(low<high)
+        {
+            int sum = numbers[low]+numbers[high];
+            if(sum == target)
+                return new int[]{low+1, high+1};
+            else if(sum < target)
+                low++;
+            else
+                high--;
+        }
+        
+        return new int[]{-1};
+    }
+}
