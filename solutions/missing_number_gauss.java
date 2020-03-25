1d07efa56578fd3bcfe59b89d9a92bb07db8c9fb
Rahul Barhate <rahulbarhate97@gmail.com>
2020-03-25 10:27:40 +0530

    Create missing_number_gauss.java

---
+++ solutions/missing_number_gauss.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int missingNumber(int[] nums)
+    {
+        int expectedSum = nums.length * (nums.length + 1)/2;
+        int actualSum = 0;
+        
+        for(int num: nums)
+            actualSum += num;
+        
+        return expectedSum - actualSum;
+    }
+}
