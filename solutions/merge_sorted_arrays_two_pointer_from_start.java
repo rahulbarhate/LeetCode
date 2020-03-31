576d1770409ee97050929645e4a650c8cfa6004c
Rahul Barhate <rahulbarhate97@gmail.com>
2020-04-01 01:20:59 +0530

    Create merge_sorted_arrays_two_pointer_from_start.java

---
+++ solutions/merge_sorted_arrays_two_pointer_from_start.java
@@ -0,0 +1,26 @@
+class Solution 
+{
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int[] nums1_copy = new int[m];
+        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+        
+        // pointers for nums1_copy and nums2
+        int p1=0, p2=0;
+        
+        //pointer for nums1
+        int p = 0;
+        
+        while((p1<m) && (p2<n))
+        {
+            nums1[p] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+            p++;
+            
+        }
+        
+        if (p1 < m)
+            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+        if (p2 < n)
+            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+    }
+}
