88a7b43f7231f3fe8ad1a6949fe5bc0dd0bc5809
Rahul Barhate <rahulbarhate97@gmail.com>
2020-04-03 08:10:37 +0530

    Create merge_sorted_arrays_two_pointer_from_end.java

---
+++ solutions/merge_sorted_arrays_two_pointer_from_end.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+  public void merge(int[] nums1, int m, int[] nums2, int n)
+  {
+    // two get pointers for nums1 and nums2
+    int p1 = m - 1;
+    int p2 = n - 1;
+    // set pointer for nums1
+    int p = m + n - 1;
+
+    // while there are still elements to compare
+    while ((p1 >= 0) && (p2 >= 0))
+      // compare two elements from nums1 and nums2 
+      // and add the largest one in nums1 
+      nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+    // add missing elements from nums2
+    System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+  }
+}
