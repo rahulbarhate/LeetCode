7c7f72a303b0091cd786a6299cd01bb86218c927
Rahul Barhate <rahulbarhate97@gmail.com>
2020-04-06 06:33:13 +0530

    Create intersection_of_two_arrays_ii_hashmap.java

---
+++ solutions/intersection_of_two_arrays_ii_hashmap.java
@@ -0,0 +1,30 @@
+class Solution
+{
+    public int[] intersect(int[] nums1, int[] nums2)
+    {
+        if(nums1.length > nums2.length)
+        {
+            return intersect(nums2, nums1);
+        }
+        
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int n:nums1)
+        {
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+        
+        
+        int k = 0;
+        for(int n:nums2)
+        {
+            int count = map.getOrDefault(n, 0);
+            if(count > 0)
+            {
+                nums1[k++] = n;
+                map.put(n, count-1);
+            }
+        }
+        
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
