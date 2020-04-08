cfe9afb4a453055cf09793ec52a4fd52b454a567
Rahul Barhate <rahulbarhate97@gmail.com>
2020-04-08 17:44:31 +0530

    Update power_of_three_maths.java

--- solutions/power_of_three_maths.java
+++ solutions/power_of_three_maths.java
@@ -2,6 +2,6 @@ class Solution
 {
     public boolean isPowerOfThree(int n)
     {
-        return (Math.log10(n / Math.log10(3)) % 1 == 0;
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
     }
 }
