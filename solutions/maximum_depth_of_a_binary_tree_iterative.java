c3e55e5eb5b4eab4c2f0682f677368112f1605c6
Rahul Barhate <rahulbarhate97@gmail.com>
2020-04-13 02:14:50 +0530

    Update maximum_depth_of_a_binary_tree_iterative.java

--- solutions/maximum_depth_of_a_binary_tree_iterative.java
+++ solutions/maximum_depth_of_a_binary_tree_iterative.java
@@ -11,24 +11,24 @@ class Solution
 {
     public int maxDepth(TreeNode root) 
     {
-        if (root == null) return 0;
+        if (root == null) 
+            return 0;
         int depth = 0;
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        int step = 0;
-        while (!q.isEmpty())
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty())
         {
-        	int s = q.size();
-        	while (s -- > 0) 
+        	int size = queue.size();
+        	while (size -- > 0) 
             {
-        		TreeNode curr = q.poll();
-        		if (curr.left != null)
-                    q.add(curr.left);
-        		if (curr.right != null)
-                    q.add(curr.right);
+        		TreeNode node = queue.poll();
+        		if (node.left != null)
+                    queue.add(node.left);
+        		if (node.right != null)
+                    queue.add(node.right);
         	}
-        	step++;
+        	depth++;
         }
-        return step;
+        return depth;
     }
 }
