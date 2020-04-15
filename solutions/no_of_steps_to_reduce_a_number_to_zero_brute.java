147db72dd5d5735cce6b63d202f591b14ff06936
Rahul Barhate <rahulbarhate97@gmail.com>
2020-04-15 20:43:47 +0530

    Create no_of_steps_to_reduce_a_number_to_zero_brute.java

---
+++ solutions/no_of_steps_to_reduce_a_number_to_zero_brute.java
@@ -0,0 +1,23 @@
+class Solution
+{
+    public int numberOfSteps (int num) 
+    {
+        int steps = 0;
+        while(num != 0)
+        {
+            if(num % 2 == 0)
+            {
+                num = num/2;
+            }
+            
+            else
+            {
+                num = num - 1;
+            }
+            
+            steps++;
+        }
+        
+        return steps;
+    }
+}
