a5c6388030128016acb5eb041226084af0af8f0c
Rahul Barhate <rahulbarhate97@gmail.com>
2020-04-25 16:25:15 +0530

    Create how_many_numbers_are_smaller_than_the_current_number.java

---
+++ solutions/how_many_numbers_are_smaller_than_the_current_number.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public int[] smallerNumbersThanCurrent(int[] nums) 
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] copy = nums.clone();
+        
+        Arrays.sort(copy);
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            map.putIfAbsent(copy[i], i);
+        }
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            copy[i] = map.get(nums[i]);
+        }
+        
+        return copy;
+    }
+}
