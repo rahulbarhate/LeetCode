dd114bb620e630ad284a822d8bff5606c3d19804
Rahul Barhate <rahulbarhate97@gmail.com>
2020-05-09 14:00:46 +0530

    Create shortest_palindrome_brute.java

---
+++ solutions/shortest_palindrome_brute.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public String shortestPalindrome(String s)
+    {
+        int length = s.length();
+        int j = 0;
+        for(int i = length - 1; i>=0; i--)
+        {
+            if(s.charAt(i) == s.charAt(j))
+            {
+                j+=1;
+            }
+        }
+        
+        if(j == s.length())
+        {
+            return s;
+        }
+        
+        String suffix = s.substring(j);
+        return new StringBuffer(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
+    }
+}
