f018ed048e36732f99ecc63b9bfee7f1de986c6b
Rahul Barhate <rahulbarhate97@gmail.com>
2020-05-17 12:01:28 +0530

    Create swap_nodes_in_pairs_recursive.java

---
+++ solutions/swap_nodes_in_pairs_recursive.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution
+{
+    public ListNode swapPairs(ListNode head) 
+    {
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+        
+        firstNode.next = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+        
+        return secondNode;
+    }
+}
