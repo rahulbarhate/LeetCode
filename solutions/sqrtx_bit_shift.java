0c1252437ff9486b3643fc9273b0959b2f25a8c4
Rahul Barhate <rahulbarhate97@gmail.com>
2020-05-23 23:00:02 +0530

    Create sqrtx_bit_shift.java

---
+++ solutions/sqrtx_bit_shift.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int mySqrt(int x)
+    {
+        if(x<2)
+            return x;
+        
+        int left = mySqrt(x >> 2) << 1;
+        int right = left + 1;
+        
+        return (long)right * right > x ? left : right;
+    }
+}
