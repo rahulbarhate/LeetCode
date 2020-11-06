2ea496b7813cb43284bddd1a7483e28892f387fc
Rahul Barhate <rahulbarhate97@gmail.com>
2020-11-05 20:21:12 -0500

    Update add_two_numbers.java

--- solutions/add_two_numbers.java
+++ solutions/add_two_numbers.java
@@ -8,27 +8,28 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution
-{
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2)
-    {
+class Solution{
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
         ListNode ln1 = l1, ln2 = l2, head = null, node = null;
-        int carry = 0, remainder = 0, sum = 0;
+        int carry = 0, sum = 0, remainder = 0;
         head = node = new ListNode(0);
-        
-        while(ln1 != null || ln2 != null)
-        {
-            sum = (ln1 != null ? ln1.val : 0) + (ln2 != null ? ln2.val : 0) + carry;
-            carry = sum / 10;
+
+        while(ln1 != null || ln2 !=null){
+            
+            sum = (((ln1 !=null) ? ln1.val : 0) + ((ln2 !=null) ? ln2.val : 0)) + carry;
+            carry = sum/10;
             remainder = sum % 10;
-            node = node.next = new ListNode(remainder);
-            ln1 = (ln1 != null ? ln1.next : null);
-            ln2 = (ln2 != null ? ln2.next : null);
+            node.next = new ListNode(remainder);
+            node = node.next;
+            ln1 = (ln1 != null) ? ln1.next : null;
+            ln2 = (ln2 != null) ? ln2.next : null;
         }
-        
-        if(carry > 0)
-            node = node.next = new ListNode(carry);
-        
+
+        if(carry > 0){
+            node.next = new ListNode(carry);
+        }
+
         return head.next;
+        
     }
 }
